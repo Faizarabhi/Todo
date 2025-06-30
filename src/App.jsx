919065aed270116017a9ca56afd42b1d50acc258
Faiza RABHI <93930498+Faizarabhi@users.noreply.github.com>
2025-06-30 14:30:08 +0100

    feat: fetch and insert todos from Supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,59 @@
-import './App.css'
+import React, { useEffect, useState } from 'react'
+import supabase from '../supabase-client'
+
 function App() {
+  const [todoList, setTodoList] = useState([])
+  const [newTodo, setNewTodo] = useState("")
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  const fetchTodos = async () => {
+    const { data, error } = await supabase.from('todos').select('*')
+    if (error) {
+      console.error("Error fetching todos:", error)
+    } else {
+      setTodoList(data)
+    }
+  }
+
+  const addTodo = async () => {
+    const newTodoData = {
+      name: newTodo,
+      is_completed: false,
+    }
+
+    const { data, error } = await supabase
+      .from('todos')
+      .insert([newTodoData])
+      .single()
+
+    if (error) {
+      console.error("Error adding todo:", error)
+    } else {
+      setTodoList([...todoList, data])
+      setNewTodo("")
+    }
+  }
 
   return (
-    <>
-    </>
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        placeholder="New Todo.."
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={addTodo}>Add To do</button>
+
+      <ul>
+        {todoList.map(todo => (
+          <li key={todo.id}>{todo.name}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
